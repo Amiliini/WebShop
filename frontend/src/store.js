f8f00f6b48e478d354a785bdce207ac38ffbcb09
Mari Sinkkonen <amiliini@hotmail.com>
2022-01-07 15:22:10 +0200

    updating profile works

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -15,6 +15,8 @@ import {
   userDetailsReducer,
   userRegisterReducer,
   userSigninReducer,
+  userUpdateProfileReducer,
+
 } from "./reducers/userReducers";
 
 const initialState = {
@@ -46,6 +48,7 @@ const reducer = combineReducers({
   orderPay: orderPayReducer,
   orderMineList: orderMineListReducer,
   userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
